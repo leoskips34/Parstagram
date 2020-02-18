6258728190297989f491fc6d7c9d1e2afed3e86c
leoskips34 <leo.samuelson@gmail.com>
2020-02-17 20:12:14 -0800

    parstagram

--- Parstagram/CameraViewController.swift
+++ Parstagram/CameraViewController.swift
@@ -27,26 +27,25 @@ class CameraViewController: UIViewController, UIImagePickerControllerDelegate {
     
     @IBAction func onCameraButton(_ sender: Any) {
     let picker = UIImagePickerController()
-       
-    picker.allowsEditing = true
-
-    if UIImagePickerController.isSourceTypeAvailable(.camera) {
-    picker.sourceType = .camera
-    } else {
-    picker.sourceType = .photoLibrary
-}
+        picker.allowsEditing = true
         
+        if UIImagePickerController.isSourceTypeAvailable(.camera) {
+            picker.sourceType = .camera
+        } else {
+            picker.sourceType = .photoLibrary
+        }
+        
+        present(picker, animated: true, completion: nil)
+    }
+    
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         let image = info[.editedImage] as! UIImage
-            
+        
         let size = CGSize(width: 300, height: 300)
         let scaledImage = image.af_imageAspectScaled(toFill: size)
-            
+        
         imageView.image = scaledImage
         
-        
         dismiss(animated: true, completion: nil)
-        }
-   }
-
+    }
 }
